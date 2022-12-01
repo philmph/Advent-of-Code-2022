991c40c6c681456a2a9703dc0146e701f0f8c607
Philipp Maier <git@pmaier.at>
2022-12-01 22:17:27 +0100

    Update to static script instead of function

--- 1-1/1.py
+++ 1-1/1.py
@@ -1,24 +1,21 @@
-def calorie_counting(file_name: str):
-    highest: int = 0
-    current: int = 0
-    sums: list(int) = []
+file_name: str = "./input"
+highest: int = 0
+current: int = 0
+sums: list[int] = []
 
-    with open(file_name, "r") as file:
-        for line in file:
-            formated_line = line.rstrip()
+with open(file_name, "r") as file:
+    for line in file:
+        formated_line = line.rstrip()
 
-            # Check if string is empty
-            if (not formated_line):
-                # Add to list and reset counter
-                sums.append(current)
-                current = 0
-            else:
-                current += int(formated_line)
+        # Check if string is empty
+        if (not formated_line):
+            # Add to list and reset counter
+            sums.append(current)
+            current = 0
+        else:
+            current += int(formated_line)
 
-    # Add final calculation
-    sums.append(current)
+# Add final calculation
+sums.append(current)
 
-    return max(sums)
-
-
-print(calorie_counting("./input"))
+print(max(sums))
