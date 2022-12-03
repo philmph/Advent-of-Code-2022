bc21bcb1b1cefda65a24a4ede76fd2f396b481c9
Philipp Maier <git@pmaier.at>
2022-12-03 22:27:57 +0100

    Add base files

---
+++ 3/main.py
@@ -0,0 +1,9 @@
+import functions
+
+file_name: str = "./testfile"
+sums: list[int] = []
+
+with open(file_name, "r") as file:
+    for line in file:
+        formated_line = line.rstrip()
+        sums.append()
