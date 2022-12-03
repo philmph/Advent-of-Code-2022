080f3b67b7b93b2af1fdea582b002e58b49670df
Philipp Maier <git@pmaier.at>
2022-12-03 22:58:33 +0100

    Add solution for day 3 challenge 1

--- 3/functions.py
+++ 3/functions.py
@@ -4,3 +4,14 @@ def get_priority(letter: str) -> int:
     # Index works like arrays, pos 1 = index 0, add 1 to calc
     output: int = letters.index(letter) + 1
     return output
+
+
+def find_common_compartment_item(rucksack_content: str) -> str:
+    rucksack_content_length_half: int = len(rucksack_content) // 2
+
+    compartment_1 = set(rucksack_content[:rucksack_content_length_half])
+    compartment_2 = rucksack_content[rucksack_content_length_half:]
+
+    for item in compartment_1:
+        if (item in compartment_2):
+            return item
--- 3/main.py
+++ 3/main.py
@@ -1,9 +1,13 @@
 import functions
 
-file_name: str = "./testfile"
+file_name: str = "./input"
 sums: list[int] = []
 
 with open(file_name, "r") as file:
     for line in file:
         formated_line = line.rstrip()
-        sums.append()
+        letter = functions.find_common_compartment_item(formated_line)
+        priority = functions.get_priority(letter)
+        sums.append(priority)
+
+print(sum(sums))
