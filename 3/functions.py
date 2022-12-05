fac06f381af1f1fb353c827d2d92564e22fb2eb7
Philipp Maier <git@pmaier.at>
2022-12-05 18:02:51 +0100

    Add learning things

--- 3/functions.py
+++ 3/functions.py
@@ -12,17 +12,23 @@ def find_common_compartment_item(rucksack_content: str) -> str:
     compartment_1 = set(rucksack_content[:rucksack_content_length_half])
     compartment_2 = rucksack_content[rucksack_content_length_half:]
 
-    for item in compartment_1:
-        if (item in compartment_2):
-            return item
+    # Alternative implementations to learn
+    list(filter(lambda item: item in compartment_2, compartment_1))
+
+
+def filter1(function, array):
+    y = []
+    for x in array:
+        if function(x):
+            y += x
+    return y
 
 
 def find_common_group_badge(rucksack_contents: list[str]) -> str:
     assert len(rucksack_contents) == 3, "rucksack_contents is not equal to 3"
     for item in set(rucksack_contents[0]):
-        if (item in rucksack_contents[1]):
-            if (item in rucksack_contents[2]):
-                return item
+        if item in rucksack_contents[1] and item in rucksack_contents[2]:
+            return item
 
 
 def split_list_every_n(l: list[str], n: int) -> list[list[str]]:
