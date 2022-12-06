a4fde1d9f175b79eabda1f891acb2da9e019ca9e
Philipp Maier <git@pmaier.at>
2022-12-06 21:14:32 +0100

    Add solution for 6/1

---
+++ 6/main.py
@@ -0,0 +1,20 @@
+def load_inputfile_contents(filepath: str) -> list[str]:
+    output: list[str] = []
+    with open(filepath, "r") as file:
+        for line in file:
+            formated_line = line.rstrip()
+            output.append(formated_line)
+
+    return output
+
+
+def find_unique_sequence_of_n(s, n):
+    for i in range(0, len(s)):
+        if len(set(s[i:i + 4])) == n:
+            return i
+
+
+filecontent = load_inputfile_contents("./6/input")
+
+output = find_unique_sequence_of_n(filecontent[0], 4) + 4
+print(output)
