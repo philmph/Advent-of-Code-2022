d74dc707895b119f28d98e708bd2caf890daa85a
Philipp Maier <git@pmaier.at>
2022-12-06 21:17:31 +0100

    And for part 2

--- 6/main.py
+++ 6/main.py
@@ -10,11 +10,14 @@ def load_inputfile_contents(filepath: str) -> list[str]:
 
 def find_unique_sequence_of_n(s, n):
     for i in range(0, len(s)):
-        if len(set(s[i:i + 4])) == n:
+        if len(set(s[i:i + n])) == n:
             return i
 
 
 filecontent = load_inputfile_contents("./6/input")
 
 output = find_unique_sequence_of_n(filecontent[0], 4) + 4
+print(output)
+
+output = find_unique_sequence_of_n(filecontent[0], 14) + 14
 print(output)
